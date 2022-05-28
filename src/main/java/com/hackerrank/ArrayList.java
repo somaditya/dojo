92cc3897e20a8b5b3c5c61c61c876d57036983cb
Somaditya Basak <somaditya.b@gmail.com>
2022-05-28 23:54:00 +0530

    hackerrank arraylist

--- src/main/java/com/hackerrank/ArrayList.java
+++ src/main/java/com/hackerrank/ArrayList.java
@@ -1,4 +1,43 @@
 package com.hackerrank;
 
+import java.io.*;
+import java.util.*;
+
 public class ArrayList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            int d = sc.nextInt();
+            List<Integer> line = new ArrayList<>();
+
+            if (d == 0) {
+                list.add(null);
+                continue;
+            } else {
+                for (int j = 1; j <= d; j++) {
+                    line.add(sc.nextInt());
+                }
+                list.add(line);
+            }
+        }
+
+        int q = sc.nextInt();
+
+        for (int i = 1; i <= q; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            try {
+                System.out.println(list.get(x - 1).get(y - 1));
+            } catch (RuntimeException e) {
+                System.out.println("ERROR!");
+            }
+        }
+    }
 }
+
