6551719f9d2c90b1aabea927de36eba579ae0792
Somaditya Basak <somaditya.b@gmail.com>
2022-05-29 15:13:17 +0530

    hackerrank varargs

---
+++ src/main/java/com/hackerrank/VarArgs.java
@@ -0,0 +1,67 @@
+package com.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+class Add {
+
+    public void add(int...x) {
+        int sum = 0;
+        StringBuilder output = new StringBuilder();
+
+        for (int i : x) {
+            sum += i;
+            output.append(i).append("+");
+        }
+
+        output.deleteCharAt(output.lastIndexOf("+"));
+        output.append("=").append(sum);
+
+        System.out.println(output);
+    }
+}
+
+
+public class VarArgs {
+
+    public static void main(String[] args) {
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            int n1=Integer.parseInt(br.readLine());
+            int n2=Integer.parseInt(br.readLine());
+            int n3=Integer.parseInt(br.readLine());
+            int n4=Integer.parseInt(br.readLine());
+            int n5=Integer.parseInt(br.readLine());
+            int n6=Integer.parseInt(br.readLine());
+            Add ob=new Add();
+            ob.add(n1,n2);
+            ob.add(n1,n2,n3);
+            ob.add(n1,n2,n3,n4,n5);
+            ob.add(n1,n2,n3,n4,n5,n6);
+            Method[] methods=Add.class.getDeclaredMethods();
+            Set<String> set=new HashSet<>();
+            boolean overload=false;
+
+            for (Method method : methods) {
+                if (set.contains(method.getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(method.getName());
+
+            }
+            if(overload)
+            {
+                throw new Exception("Overloading not allowed");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
+
