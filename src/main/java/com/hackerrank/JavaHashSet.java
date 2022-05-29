8fa1555d347d9cd87cc8382bcda9f52cd55d9bd3
Somaditya Basak <somaditya.b@gmail.com>
2022-05-29 15:34:52 +0530

    hackerrank java hash set

---
+++ src/main/java/com/hackerrank/JavaHashSet.java
@@ -0,0 +1,27 @@
+package com.hackerrank;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class JavaHashSet {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        Set<String> pairs = new HashSet<>(t);
+
+        for (int i = 0; i < t; i ++) {
+            pairs.add("(" + pair_left[i] + "," + pair_right[i] + ")");
+            System.out.println(pairs.size());
+        }
+    }
+}
