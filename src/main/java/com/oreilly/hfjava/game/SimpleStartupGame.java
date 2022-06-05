1e6c113f0e8b44db4d0ed1cecfe5e1db1898f014
Somaditya Basak <somaditya.b@gmail.com>
2022-06-06 00:22:31 +0530

    code formatting

--- src/main/java/com/oreilly/hfjava/game/SimpleStartupGame.java
+++ src/main/java/com/oreilly/hfjava/game/SimpleStartupGame.java
@@ -4,24 +4,18 @@ public class SimpleStartupGame {
 
     public static void main(String[] args) {
         int numberOfGuesses = 0;
-
         GameHelper helper = new GameHelper();
-
         SimpleStartup startup = new SimpleStartup();
 
         int randomNum = (int) (Math.random() * 5);
-
         int[] locations = {randomNum, randomNum + 1, randomNum + 2};
-
         startup.setLocationCells(locations);
 
         boolean isAlive = true;
-
+        
         while (isAlive) {
             String guess = helper.getUserInput("enter a number");
-
             String result = startup.checkYourself(guess);
-
             numberOfGuesses++;
 
             if (result.equals("kill")) {
