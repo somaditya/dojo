d35c64f82d2ebf61811bb3af1686cb5acfc18f9c
Somaditya Basak <somaditya.b@gmail.com>
2022-08-14 00:05:55 +0530

    word count

--- src/main/java/com/udacity/WordCount.java
+++ src/main/java/com/udacity/WordCount.java
@@ -1,8 +1,23 @@
 package com.udacity;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class WordCount {
 
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception {
+        
+        File file = new File("src/main/java/com/udacity/98-0.txt");
+
+        Scanner scan = new Scanner(file);
+
+        int wordCount = 0;
+
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            wordCount += line.split(" ").length;
+        }   
+
+        System.out.println("Word count: " + wordCount);
     }
 }
