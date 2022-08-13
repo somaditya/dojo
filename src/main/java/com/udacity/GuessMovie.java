f393ba49ad42652ceb0ff94c173eb7cf5c9b3f1e
Somaditya Basak <somaditya.b@gmail.com>
2022-08-14 02:34:14 +0530

    guess movie complete

--- src/main/java/com/udacity/GuessMovie.java
+++ src/main/java/com/udacity/GuessMovie.java
@@ -1,9 +1,7 @@
 package com.udacity;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class GuessMovie {
     public static void main(String[] args) throws Exception {
@@ -16,16 +14,53 @@ public class GuessMovie {
             movies.add(scan.nextLine());
         }
 
-        int random = (int) Math.random() * movies.size();
-
+        int random = (int) (Math.random() * movies.size());
         String movie = movies.get(random);
-
-        int length = movie.length();
+        int spaces = 0;
 
         for (char c : movie.toCharArray()) {
-            System.out.print("_");
+            if (c == ' ') {
+                spaces++;
+            }
         }
 
-        
+        Set<Character> wrongGuesses = new HashSet<>();
+        int right = 0;
+        boolean[] correct = new boolean[100];
+
+        while (right < (movie.length() - spaces)) {
+            System.out.println("You are guessing:");
+
+            for (int i = 0; i < movie.length(); i++) {
+                if (correct[i]) {
+                    System.out.print(movie.toCharArray()[i]);
+                } else {
+                    System.out.print("_");
+                }
+            }
+
+            System.out.println();
+            System.out.println("You have guessed " + wrongGuesses.size() + " wrong letters.");
+            System.out.println("Guess a letter: ");
+
+            Scanner scanner = new Scanner(System.in);
+            String guess = scanner.next();
+            boolean found = false;
+
+            for (int i = 0; i < movie.length(); i++) {
+                if ((guess.charAt(0) == movie.charAt(i)) && (correct[i] == false)) {
+                    correct[i] = true;
+                    right++;
+                    found = true;
+                }
+            }
+
+            if (!found && !wrongGuesses.contains(guess.charAt(0))) {
+                wrongGuesses.add(guess.charAt(0));
+            }
+        }
+
+        System.out.println("You win!");
+        System.out.printf("You have guessed '%s' correctly.", movie);
     }
 }
