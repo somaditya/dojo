337a3fdf708573f837500fd4b07505997c5bde12
Somaditya Basak <somaditya.b@gmail.com>
2022-08-14 00:40:51 +0530

    guess movie wip

---
+++ src/main/java/com/udacity/GuessMovie.java
@@ -0,0 +1,31 @@
+package com.udacity;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GuessMovie {
+    public static void main(String[] args) throws Exception {
+
+        File file = new File("src/main/java/com/udacity/movies.txt");
+        Scanner scan = new Scanner(file);
+        List<String> movies = new ArrayList<>();
+
+        while (scan.hasNextLine()) {
+            movies.add(scan.nextLine());
+        }
+
+        int random = (int) Math.random() * movies.size();
+
+        String movie = movies.get(random);
+
+        int length = movie.length();
+
+        for (char c : movie.toCharArray()) {
+            System.out.print("_");
+        }
+
+        
+    }
+}
