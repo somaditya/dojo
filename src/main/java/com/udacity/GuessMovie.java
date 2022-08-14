6bad0b32e43cbe126f67506800277702456bf1d5
Somaditya Basak <somaditya.b@gmail.com>
2022-08-15 00:21:05 +0530

    udacity space challenge: item

--- src/main/java/com/udacity/GuessMovie.java
+++ src/main/java/com/udacity/GuessMovie.java
@@ -26,6 +26,6 @@ public class GuessMovie {
             System.out.print("_");
         }
 
-        
+
     }
 }
---
+++ src/main/java/com/udacity/spacechallenge/Item.java
@@ -0,0 +1,22 @@
+package com.udacity.spacechallenge;
+
+public class Item {
+    private String name;
+    private int weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
