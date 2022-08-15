21ef4f8d8a74e38606c7cf80e42c6fe66b656c5a
Somaditya Basak <somaditya.b@gmail.com>
2022-08-16 02:24:22 +0530

    generics

---
+++ src/main/java/com/udacity/nd079/generics/GenericsExample.java
@@ -0,0 +1,27 @@
+package com.udacity.nd079.generics;
+
+import java.util.ArrayList;
+
+public class GenericsExample {
+    public static void main(String[] args) {
+        ArrayList<Object> variables = new ArrayList<>();
+
+        Double doubleNumber = 1.5;
+        String name = "Sally";
+        Integer intNumber = 1;
+        Character letter = 'a';
+
+        variables.add(doubleNumber);
+        variables.add(name);
+        variables.add(intNumber);
+        variables.add(letter);
+
+        for (Object o : variables) {
+            displayClassName(o);
+        }
+    }
+
+    public static <T> void displayClassName(T variable) {
+        System.out.println(variable.getClass().getName());
+    }
+}
