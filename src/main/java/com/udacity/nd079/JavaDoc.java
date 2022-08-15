d5e6b1a5b3f200ebad10f3b8e4b8148024f8f710
Somaditya Basak <somaditya.b@gmail.com>
2022-08-15 20:26:01 +0530

    JavaDoc

---
+++ src/main/java/com/udacity/nd079/JavaDoc.java
@@ -0,0 +1,20 @@
+package com.udacity.nd079;
+
+/**
+ * Example class for JavaDoc
+ *
+ * @author somaditya.basak
+ */
+public class JavaDoc {
+
+    /**
+     * Example method for JavaDoc
+     *
+     * @param number - Input number
+     * @param name - Input name
+     * @return - String Result
+     */
+    public String someMethod(int number, String name) {
+        return "Result";
+    }
+}
