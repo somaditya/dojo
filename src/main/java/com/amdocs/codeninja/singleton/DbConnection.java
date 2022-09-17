f882e0aa8677ad949990858c560a353266d9cad0
Somaditya Basak <somaditya.b@gmail.com>
2022-09-18 00:04:57 +0530

    singleton

---
+++ src/main/java/com/amdocs/codeninja/singleton/DbConnection.java
@@ -0,0 +1,33 @@
+package com.amdocs.codeninja.singleton;
+
+public final class DbConnection {
+    private static DbConnection instance;
+
+    private String connectionString;
+
+    private DbConnection(String connectionString) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        this.connectionString = connectionString;
+    }
+
+    public static DbConnection getInstance(String connectionString) {
+        if (instance == null) {
+            instance = new DbConnection(connectionString);
+        }
+
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        DbConnection connection1 = DbConnection.getInstance("foo");
+        DbConnection connection2 = DbConnection.getInstance("bar");
+
+        System.out.println(connection1.connectionString);
+        System.out.println(connection2.connectionString);
+    }
+}
