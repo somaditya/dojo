7cd437ff2ffaa7e05a851ec23050daf31faedd4a
Somaditya Basak <mail@somaditya.com>
2024-01-21 01:06:04 +0530

    HW06 cat complete

--- src/main/java/edu/gatech/cs1331/hw06/Cat.java
+++ src/main/java/edu/gatech/cs1331/hw06/Cat.java
@@ -1,5 +1,51 @@
 package edu.gatech.cs1331.hw06;
 
-public class Cat {
+public class Cat extends Pet {
+    private int miceCaught;
 
+    public Cat(String name, double health, int painLevel, int miceCaught) {
+        super(name, health, painLevel);
+        this.miceCaught = Math.max(miceCaught, 0);
+    }
+
+    public Cat(String name, double health, int painLevel) {
+        this(name, health, painLevel, 0);
+    }
+
+    public int getMiceCaught() {
+        return miceCaught;
+    }
+
+    @Override
+    public int treat() {
+        int tTime;
+
+        if (miceCaught < 4) {
+            tTime = (int) Math.ceil((getPainLevel() * 2) / getHealth());
+        } else if (miceCaught <= 7) {
+            tTime = (int) Math.ceil(getPainLevel() / getHealth());
+        } else {
+            tTime = (int) Math.ceil(getPainLevel() / (getHealth() * 2));
+        }
+
+        heal();
+
+        return tTime;
+    }
+
+    public void speak() {
+        super.speak();
+        String sound = (getPainLevel() > 5) ? "MEOW" : "meow";
+        String out = "";
+
+        for (int i = 1; i <= miceCaught; i++) {
+            out += sound;
+        }
+
+        System.out.println(out.trim());
+    }
+
+    public boolean equals(Object o) {
+        return (super.equals(o) && (((Cat) o).miceCaught == miceCaught));
+    }
 }
