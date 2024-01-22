e450cb4763e7e8f28666aa7ae0e2ea238606d841
Somaditya Basak <mail@somaditya.com>
2024-01-22 06:26:51 +0530

    test method

--- src/main/java/edu/gatech/cs1331/tests/TestQuestion.java
+++ src/main/java/edu/gatech/cs1331/tests/TestQuestion.java
@@ -1,14 +1,24 @@
 package edu.gatech.cs1331.tests;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class TestQuestion {
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
         for (int i = 0; i < 10_000; i++) {
             Random random = new Random();
             System.out.println(random.nextInt(8) + 3);
+        }
+    }
+
+    public static void letsGetEven(ArrayList<Integer> nums) {
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (nums.get(i) / 2 == 0) {
+                nums.remove(i);
+            }
         }
     }
 }
