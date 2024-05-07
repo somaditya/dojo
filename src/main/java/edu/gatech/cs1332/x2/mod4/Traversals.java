b6435ba0b9613c36a77e7c7edbdd898544f21571
Somaditya Basak <somaditya.b@gmail.com>
2024-05-08 05:20:56 +0530

    Update Traversals.java

--- src/main/java/edu/gatech/cs1332/x2/mod4/Traversals.java
+++ src/main/java/edu/gatech/cs1332/x2/mod4/Traversals.java
@@ -17,72 +17,60 @@ public class Traversals<T extends Comparable<? super T>> {
      * Given the root of a binary search tree, generate a
      * pre-order traversal of the tree. The original tree
      * should not be modified in any way.
-     *
+     * <p>
      * This must be done recursively.
-     *
+     * <p>
      * Must be O(n).
      *
-     * @param <T> Generic type.
+     * @param <T>  Generic type.
      * @param root The root of a BST.
      * @return List containing the pre-order traversal of the tree.
      */
     public List<T> preorder(TreeNode<T> root) {
-        if (root != null) {
-            List<T> list = new LinkedList<>();
-            preorderHelper(root, list);
+        List<T> list = new LinkedList<>();
+        preorderHelper(root, list);
 
-            return list;
-        }
-
-        return null;
+        return list;
     }
 
     /**
      * Given the root of a binary search tree, generate an
      * in-order traversal of the tree. The original tree
      * should not be modified in any way.
-     *
+     * <p>
      * This must be done recursively.
-     *
+     * <p>
      * Must be O(n).
      *
-     * @param <T> Generic type.
+     * @param <T>  Generic type.
      * @param root The root of a BST.
      * @return List containing the in-order traversal of the tree.
      */
     public List<T> inorder(TreeNode<T> root) {
-        if (root != null) {
-            List<T> list = new LinkedList<>();
-            inorderHelper(root, list);
+        List<T> list = new LinkedList<>();
+        inorderHelper(root, list);
 
-            return list;
-        }
-
-        return null;
+        return list;
     }
 
     /**
      * Given the root of a binary search tree, generate a
      * post-order traversal of the tree. The original tree
      * should not be modified in any way.
-     *
+     * <p>
      * This must be done recursively.
-     *
+     * <p>
      * Must be O(n).
      *
-     * @param <T> Generic type.
+     * @param <T>  Generic type.
      * @param root The root of a BST.
      * @return List containing the post-order traversal of the tree.
      */
     public List<T> postorder(TreeNode<T> root) {
-        if (root != null) {
-            List<T> list = new LinkedList<>();
-            postorderHelper(root, list);
+        List<T> list = new LinkedList<>();
+        postorderHelper(root, list);
 
-            return list;
-        }
-
-        return null;
+        return list;
     }
 
     private void preorderHelper(TreeNode<T> node, List<T> list) {
